561085856e70269888239fe330fc508bba24c863
Your Name01~ <100568880+kjm0810@users.noreply.github.com>
2026-03-12 23:11:30 +0900

    수정

---
+++ pages/api/searchAnalytics/research.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import db from '@/utils/db';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    try {
+        const rows = await db.query(
+            'INSERT INTO research (id) VALUES ($1)',
+            [req.body.id]
+        );
+        return res.status(200).json(rows); // ✅ 반드시 res로 응답
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'DB error' });
+    }
+}
